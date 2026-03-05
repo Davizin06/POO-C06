f6e78a05847c6279fba3da8bbf883d599fe0d0b3
Davi Marcondes <152312917+Davizin06@users.noreply.github.com>
2026-03-04 21:42:37 -0300

    Exercicio 3

--- POO/src/Exercicios/Exercicio3.java
+++ POO/src/Exercicios/Exercicio3.java
@@ -1,5 +1,22 @@
 package Exercicios;
 
-public class Exercicio3 {
+import java.util.Scanner;
 
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        System.out.println("Quantos lanches foram consumidos? ");
+        int quantidade = cin.nextInt();
+        int soma = 0;
+
+        for(int i = 1; i <= quantidade; ++i) {
+            System.out.println("Digite a quantidade de lanches da pessoa " + i + ": ");
+            int lanches = cin.nextInt();
+            soma += lanches;
+        }
+
+        double media = (double)soma / (double)quantidade;
+        System.out.println("Soma total de lanches: " + soma);
+        System.out.println("Média de lanches: " + media);
+    }
 }
